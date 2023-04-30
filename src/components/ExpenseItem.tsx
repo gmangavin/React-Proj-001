b62c37e4dc82e7a891a5d2c1330fd663fb785339
Gavin Hell <gmangavin1@gmail.com>
2023-04-30 12:12:45 -0700

    ExpenseItem Update #1

--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -1,5 +1,13 @@
 function ExpenseItem() {
-    return <h2>Expense item!</h2>
+    return (
+        <div>
+            <h2>April 30th 2023</h2>
+            <div>
+                <h2>Energy Bill</h2>
+                <h2>$231.45</h2>
+            </div>
+        </div>
+    )
 }
 
 export default ExpenseItem;
