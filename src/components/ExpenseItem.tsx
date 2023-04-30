a1d4fcac7890b7d8e7f9839aecac230f77bdc231
Gavin Hell <gmangavin1@gmail.com>
2023-04-30 12:21:23 -0700

    CSS TSX update

--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -2,11 +2,11 @@ import './ExpenseItem.css'
 
 function ExpenseItem() {
     return (
-        <div className='expense-item'>
+        <div className="expense-item">
             <h2>April 30th 2023</h2>
             <div className="expense-item__description">
                 <h2>Energy Bill</h2>
-                <h2>$231.45</h2>
+                <h2 className="expense-item__price">$231.45</h2>
             </div>
         </div>
     )
