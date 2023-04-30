0eab7d81096b8c4ec628a7baec2b6585c5e279d8
Gavin Hell <gmangavin1@gmail.com>
2023-04-30 12:55:01 -0700

    helper const w/ month, day, and year

--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -1,14 +1,17 @@
 import './ExpenseItem.css'
 
 function ExpenseItem(props: {date: Date, title: string, amount: number}) {
+    const month = props.date.toLocaleString('en-US', {month: 'long'});
+    const day = props.date.toLocaleString('en-US', {day: '2-digit'});
+    const year = props.date.getFullYear();
 
     //Being expenseDate is a javascript object we can use the toISOString() method to convert it to a string
     return (
         <div className="expense-item">
             <div>
-                <div>{props.date.toLocaleString('en-US', {month: 'long'})}</div>
-                <div>Day</div>
-                <div>Year</div>
+                <div>{month}</div>
+                <div>{day}</div>
+                <div>{year}</div>
             </div>
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
