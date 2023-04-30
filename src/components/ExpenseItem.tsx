e9af606bfd45b48f2564cbf306b229b0728f5089
Gavin Hell <gmangavin1@gmail.com>
2023-04-30 12:52:12 -0700

    Extracting clean month format

--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -5,7 +5,11 @@ function ExpenseItem(props: {date: Date, title: string, amount: number}) {
     //Being expenseDate is a javascript object we can use the toISOString() method to convert it to a string
     return (
         <div className="expense-item">
-            <h2>{props.date.toISOString()}</h2>
+            <div>
+                <div>{props.date.toLocaleString('en-US', {month: 'long'})}</div>
+                <div>Day</div>
+                <div>Year</div>
+            </div>
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <h2 className="expense-item__price">{props.amount}</h2>
