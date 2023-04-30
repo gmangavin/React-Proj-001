fa13be5d5e991c0d4394d0ba97ad2cdc378ffa0f
Gavin Hell <gmangavin1@gmail.com>
2023-04-30 12:30:43 -0700

    ExpenseItem Const

--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -1,12 +1,17 @@
 import './ExpenseItem.css'
 
 function ExpenseItem() {
+
+    const expenseDate = new Date(2023, 4, 30); //This is just javascript and not react
+    const expenseTitle = 'Energy Bill';
+    const expenseAmount = 231.45;
+
     return (
         <div className="expense-item">
-            <h2>April 30th 2023</h2>
+            <h2>{expenseDate.toISOString()}</h2>
             <div className="expense-item__description">
-                <h2>Energy Bill</h2>
-                <h2 className="expense-item__price">$231.45</h2>
+                <h2>{expenseTitle}</h2>
+                <h2 className="expense-item__price">{expenseAmount}</h2>
             </div>
         </div>
     )
