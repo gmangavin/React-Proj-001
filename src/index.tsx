91b4abce459fd1f10e1bad18e0a66281722ce066
Gavin Hell <gmangavin1@gmail.com>
2023-04-30 11:57:06 -0700

    Organize CSS file

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
 
-import './index.css'
+import './styles/global.css'
 import App from './App'
 
 const rootElement = document.getElementById('root') as HTMLElement;
